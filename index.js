dabccc78af00abd2ca843f9e00f94ec6dabffb22
Jacky Liang <asheriffy@gmail.com>
2022-03-29 10:54:09 -0400

    commit tree a01c1c70ea987d8379f2d2cb1ae4959f353be5e5

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+// const fetchBooks = async () => {
+//   // To pass the tests, don't forget to return your fetch!
+//   let req = await fetch("https://anapioficeandfire.com/api/books")
+//   let res = await req.json()
+//   renderBooks(res)
+// }
+
+function fetchBooks () {
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(data => renderBooks(data))
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
